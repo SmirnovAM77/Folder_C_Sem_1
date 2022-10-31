d90185af8fe8eccce9e1164bdee3d84b5a4a4785
Artem <temik-cool@mail.ru>
2022-10-31 19:02:24 +0300

    23

---
+++ task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.Write("Введите число: ");
+int chislo = Math.Abs(Convert.ToInt32 (Console.ReadLine()));
+
+void Cubus (int chisloCub)
+{
+    int cubus = 0;
+    for (int i = 1; i <= chisloCub; i++)
+    {
+        cubus = i*i*i;
+        Console.WriteLine($"{i} | {cubus}");
+    }
+}
+
+Cubus (chislo);
