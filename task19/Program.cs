071bd1ed2372696abc886a521f14a04c32dcf494
Artem <temik-cool@mail.ru>
2022-10-31 19:58:12 +0300

    Задача 19

---
+++ task19/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+
+
+
+// Console.Write("Введите координату X первой точки: ");
+// int ax = Convert.ToInt32 (Console.ReadLine());
+// Console.Write("Введите координату Y первой точки: ");
+// int ay = Convert.ToInt32 (Console.ReadLine());
+// Console.Write("Введите координату Z первой точки: ");
+// int az = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine("___________________________________");
+// Console.Write("Введите координату X второй точки: ");
+// int bx = Convert.ToInt32 (Console.ReadLine());
+// Console.Write("Введите координату Y второй точки: ");
+// int by = Convert.ToInt32 (Console.ReadLine());
+// Console.Write("Введите координату Z второй точки: ");
+// int bz = Convert.ToInt32 (Console.ReadLine());
+
+// double Vektor (int axN, int ayN, int azN, int bxN, int byN, int bzN)
+// {
+//     double dlina = Math.Sqrt((bxN-axN)*(bxN-axN) + (byN-ayN)*(byN-ayN) + (bzN-azN)*(bzN-azN));
+//     return dlina;
+// }
+
+// double vektor = Math.Round (Vektor (ax, ay, az, bx, by, bz), 2);
+
+// Console.Write($"Расстояние между двумя точками A ({ax},{ay},{az}) и B ({bx},{by},{bz}) : {vektor}");
