fc77feaf411f3b60f676432d242f08f4b4419af4
Artem <temik-cool@mail.ru>
2022-11-11 18:55:23 +0300

    29

--- task27/Program.cs
+++ task27/Program.cs
@@ -22,28 +22,3 @@ int SummNumb (int num)
 
 int summNumb = SummNumb (a);
 Console.WriteLine($"Сумма всех цифр числа {a} -> {summNumb}");
-
-
-
-// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
-
-// Console.WriteLine("Введите число, возводимое в степень: ");
-// int a = Convert.ToInt32 (Console.ReadLine());
-// Console.WriteLine("Введите натуриальную степень данного числа: "); 
-// int aB = Convert.ToInt32 (Console.ReadLine());
-
-// int PowerNum (int num, int power)
-// {
-//     int summ = num;
-//     for (int i = 1; i < power; i++)
-//         {
-//             summ *= num;
-//         }
-//     if (power==0) summ = 1; // примем, что "0" - тоже натуральное число
-//     return summ;
-// }
-
-// int powerNum = PowerNum (a, aB);
-// Console.WriteLine(aB < 0 ? "Введите натуриальное число." : $"Число {a}, возведенное в степень {aB} - это {powerNum}");
---
+++ task29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите длину желаемого массива: ");
+int len = Convert.ToInt32 (Console.ReadLine());
+
+int[] NewArray (int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+
+void PrintArray (int[] arrayP)
+{
+    int arrayPLength = (arrayP.Length-1);
+    Console.Write("[");
+    for (int i = 0; i < arrayPLength; i++)
+    {
+        Console.Write($"{arrayP[i]}, ");
+    }   
+    Console.Write($"{arrayP[arrayPLength]}]");
+}
+
+if (len > 0)
+{
+    int[] newArray = NewArray (len);
+    PrintArray (newArray);
+}
+else
+{
+    Console.WriteLine("Введите число более 0");
+}
