f129f7109c6187ec8aa4bd960b03bb82f736c8b0
Artem <temik-cool@mail.ru>
2022-11-15 12:00:28 +0300

    Задача 25 универсал коррект

--- task25_universal/Program.cs
+++ task25_universal/Program.cs
@@ -9,18 +9,19 @@ int a = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine("Введите натуриальную степень данного числа: "); 
 int aB = Convert.ToInt32 (Console.ReadLine());
 
-int PowerNum (int num, int power)
+double PowerNum (int num, int power)
 {
     int powerM = Math.Abs(power);
-    int summ = num;
+    double summ = num;
     for (int i = 1; i < powerM; i++)
         {
             summ *= num;
         }
-    if (power==0) summ = 1; // примем, что "0" - тоже натуральное число
+    if (power==0) summ = 1;
+    if (power < 0) summ = 1 / summ;
     return summ;
 }
 
 double powerNum = PowerNum (a, aB);
-double powerNumMin = 1 / powerNum;
-Console.WriteLine(aB < 0 ? $"Число {a}, возведенное в степень {aB} - это {powerNumMin}" : $"Число {a}, возведенное в степень {aB} - это {powerNum}");
+// double powerNumMin = 1 / powerNum;
+Console.WriteLine($"Число {a}, возведенное в степень {aB} - это {powerNum}");
