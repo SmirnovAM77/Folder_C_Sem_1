35a9a9238a6d2cf6701ace29b12e0e3615a2700e
Artem <temik-cool@mail.ru>
2022-11-21 23:03:03 +0300

    Задача 34

---
+++ task34/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] array = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)
+{
+array[i] = rnd.Next(min, max + 1);
+}
+return array;
+}
+
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+else Console.Write($"{array[i]}");
+}
+Console.WriteLine("]");
+}
+
+int EvenNumbers (int[] array)
+{   
+    int numbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) numbers += 1;
+    }
+    return numbers;
+
+}
+// int[] GetSumPositiveNegativeElem(int[] array)
+// {
+// int sumPositive = 0;
+// int sumNegative = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+// if (array[i] > 0) sumPositive += array[i];
+// else sumNegative += array[i];
+// }
+
+// int[] result = {sumPositive,sumNegative};
+// return result;
+// }
+
+int[] arr = CreateArrayRndInt(12, 111, 999);
+PrintArray(arr);
+int evenNumbers = EvenNumbers(arr);
+Console.WriteLine($"Количество четных чисел в массиве - {evenNumbers}");
+// int[] sumPositiveNegativeElem = GetSumPositiveNegativeElem(arr);
+// Console.WriteLine($"Сумма положительных элементов = {sumPositiveNegativeElem[0]}");
+// Console.WriteLine($"Сумма отрицательных элементов = {sumPositiveNegativeElem[1]}");
