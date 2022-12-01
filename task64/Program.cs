66e6f95ce5b8e7fcbac9c336819bdf8c8278d402
Artem <temik-cool@mail.ru>
2022-12-01 22:02:07 +0300

    Задача 66

---
+++ task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+void NaturalNumbersToN(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num}, ");
+    NaturalNumbersToN (num - 1);
+}
+
+Console.WriteLine("Задайте натуральное число N");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number > 1)
+{
+    Console.Write($"N = {number} -> ");
+    NaturalNumbersToN(number);
+}
+else Console.WriteLine("Введите натуоальное число больше 0");
---
+++ task66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+
+int NaturalNumbersToN(int numM, int numN)
+{
+    if (numN >= numM)
+    {
+        if (numN == numM) return numN;
+        return numM + NaturalNumbersToN(numM + 1, numN);
+    }
+    else
+    {
+        if (numN == numM) return numM;
+        return numN + NaturalNumbersToN(numM, numN + 1);
+    }
+}
+
+Console.WriteLine("Задайте натуральное число M");
+int numberM = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Задайте натуральное число N");
+int numberN = Convert.ToInt32 (Console.ReadLine());
+
+if (numberM > 0 && numberN > 0)
+{
+    int naturalNumbersToN = NaturalNumbersToN (numberM, numberN);
+    Console.WriteLine($"Сумма натуральных чисел между {numberM} и {numberN} равняется {naturalNumbersToN}");
+}
+else Console.WriteLine("Введите натуоальное число больше 0");
